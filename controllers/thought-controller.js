b3d403fc60d4c80c3137e6d7f7c4476b02b2ea32
smckelli <smckelli@gmail.com>
2022-07-02 08:37:57 -0500

    still working delete route on reactions

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -156,31 +156,21 @@ const thoughtController = {
             .catch(err => res.status(400).json(err))
     },
 
-    deleteReaction({
-        params
-    }, res) {
-        Thoughts.findOneAndUpdate({
-                _id: params.thoughtId
-            }, {
-                $pull: {
-                    reactions: {
-                        reactionId: params.reactionId
-                    }
-                }
-            }, {
-                new: true
-            })
-            .then(dbThoughtData => {
-                if (!dbThoughtData) {
-                    res.status(404).json({
-                        message: 'Nope!'
-                    });
-                    return;
-                }
-                res.json(dbThoughtData);
-            })
-            .catch(err => res.json(err));
-    }
+    deleteReaction({ params }, res) {
+        Thoughts.findOneAndUpdate(
+        { _id: params.thoughtId },
+        { $pull: { reactions: params.reactionId } },
+        { new: true }
+        )
+        .then((dbThoughtData) => {
+            if (!dbThoughtData) {
+            res.status(404).json({ message: 'No thought found with this id' });
+            return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch((err) => res.status(400).json(err));
+    },
 
 };
 
